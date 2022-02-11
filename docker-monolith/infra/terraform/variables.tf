dd1e53314dcc21d9cb1c649f3981fb9c81c223ac
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-02-11 12:29:15 +0000

    Done homework #11

---
+++ docker-monolith/infra/terraform/variables.tf
@@ -0,0 +1,40 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "ru-central1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
+variable app_instance_name {
+  description = "Name of APP instance"
+}
+variable instances_count {
+  description = "Count of instances"
+  default     = 1
+}
